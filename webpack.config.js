3e98049afd0bb2ca307755185bd1521e3fc5d862
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2021-12-24 17:13:39 +0700

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path    = require("path")
+const path = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[name].js.map",
-    path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
+    mode: "production",
+    devtool: "source-map",
+    entry: {
+        application: "./app/javascript/packs/application.js"
+    },
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: "[name].js.map",
+        path: path.resolve(__dirname, "app/assets/builds"),
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        })
+    ]
 }
