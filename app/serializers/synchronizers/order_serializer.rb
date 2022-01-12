40761511ec5d1364126af9bbfb9c818480d68706
thanhdatdev <thanhdat.nguyen2104@gmail.com>
2022-01-12 16:07:56 +0700

    refactor coding

--- app/serializers/synchronizers/order_serializer.rb
+++ app/serializers/synchronizers/order_serializer.rb
@@ -11,48 +11,7 @@ module Synchronizers
       hash = []
       data.each do |obj|
         invoiceDelivery = obj['invoiceDelivery'].present?
-        case branch
-        when 'Zenfaco'
-          hash << {
-            'so_ct' =>  obj['code'],
-            'ngay_ct'=> obj['purchaseDate'],
-            'ma_kh' => obj['customerCode'],
-            't_tt_nt' => obj['total'],
-            'trang_thai' => 8, #obj['status'],
-            'dien_giai' => obj['description'],
-            'details' =>
-              obj['invoiceDetails']&.map { |invoice|
-                {
-                  'ma_vt' => invoice['productCode'],
-                  'sl_xuat' => invoice['quantity'],
-                  'gia_ban_nt' => invoice['price'],
-                  'ty_le_ck' => invoice['discountRatio'],
-                  'tien_ck_nt' => invoice['discount'],
-                  'dien_giai' => invoice['note'],
-                  'tien_hang_nt' => invoice['quantity'] * invoice['price'],
-                  'tien_nt' => (invoice['quantity'] * invoice['price']) - invoice['discount'],
-                  'ma_lvt' => "TP",
-                  'tg_tk' =>  true,
-                  'tk_vt' => "1561",
-                  'tk_dt' => "51112",
-                  'tk_gv' => "6321"
-                }},
-            'ma_van_don' => invoiceDelivery ? obj['invoiceDelivery']['deliveryCode'] : '',
-            'ten_nguoi_nhan' => invoiceDelivery ? obj['invoiceDelivery']['receiver'] : '',
-            'sdt_nguoi_nhan' => invoiceDelivery ? obj['invoiceDelivery']['contactNumber'] : '',
-            'dia_chi_nguoi_nhan' => invoiceDelivery ? obj['invoiceDelivery']['address'] : '',
-            'khu_vuc_nhan' => invoiceDelivery ? obj['invoiceDelivery']['locationId'] : '',
-            'phuong_xa' => invoiceDelivery ? obj['invoiceDelivery']['locationName'] : '',
-            'ma_kho' => obj['branchId'],
-            'thu_ho' => invoiceDelivery ? obj['invoiceDelivery']['usingPriceCod'] : false,
-            'don_vi_vc' => invoiceDelivery ? obj['invoiceDelivery']['partnerDelivery']['code'] : '',
-            'ma_kenh' => 'Kiotviet',
-            'pt_thanh_toan' => '61de7a6b5bc1556ae1e34a24',
-            'ten_pt_thanh_toan' => 'COD',
-            'nhan_vien_giao_hang' =>  obj['branchId']
-          }
-        when 'Fascom'
-          hash << {
+        default = {
             'so_ct' =>  obj['code'],
             'ngay_ct'=> obj['purchaseDate'],
             'ma_kh' => obj['customerCode'],
@@ -87,6 +46,15 @@ module Synchronizers
             'don_vi_vc' => invoiceDelivery ? obj['invoiceDelivery']['partnerDelivery']['code'] : '',
             'ma_kenh' => 'Kiotviet'
           }
+        case branch
+        when 'Zenfaco'
+          hash << default.merge!(
+              'pt_thanh_toan' => '61de7a6b5bc1556ae1e34a24',
+              'ten_pt_thanh_toan' => 'COD',
+              'nhan_vien_giao_hang' =>  obj['branchId']
+            )
+        when 'Fascom'
+          hash << default
         end
       end
       hash
